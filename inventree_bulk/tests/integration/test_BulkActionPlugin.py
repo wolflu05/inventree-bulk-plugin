248288eec157aaaa9d291e7140a04a4bde70de67
wolflu05 <76838159+wolflu05@users.noreply.github.com>
2023-01-03 19:56:13 +0100

    debug: actions

--- inventree_bulk/tests/integration/test_BulkActionPlugin.py
+++ inventree_bulk/tests/integration/test_BulkActionPlugin.py
@@ -1,6 +1,11 @@
 from django.test import TestCase
 
+from ...models import validate_template
+
 
 class BulkActionPluginTestCase(TestCase):
     def test_hello(self):
+        with self.assertRaises(Exception):
+            validate_template({})
+        print("HELLO WORLD")
         pass
