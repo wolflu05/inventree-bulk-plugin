5e38b74ae63dcb2a1e36977ca07b4e1bce4e242e
wolflu05 <76838159+wolflu05@users.noreply.github.com>
2023-01-04 00:19:54 +0100

    debug: tests

--- inventree_bulk/tests/integration/test_BulkActionPlugin.py
+++ inventree_bulk/tests/integration/test_BulkActionPlugin.py
@@ -1,11 +1,37 @@
+from django.urls import reverse
 from django.test import TestCase
+from rest_framework.test import APITestCase
+
+from plugin import registry
 
 from ...models import validate_template
+# from ...BulkActionPlugin import BulkActionPlugin
 
 
 class BulkActionPluginTestCase(TestCase):
     def test_hello(self):
         with self.assertRaises(Exception):
             validate_template({})
-        print("HELLO WORLD")
+
+
+class BulkActionPluginAPITestCase(APITestCase):
+    def test_url_parse(self):
+        print(registry.plugins)
+        print(registry.plugins_full)
+        print(registry.get_plugin('bulkaction'))
+        with self.assertRaises(Exception):
+            url = reverse("bulkaction:parse")
+            data = {
+                "version": "0.1.0",
+                "input": {},
+                "templates": [],
+                "output": {
+                    "generate": {
+                        "name": "Test",
+                        "description": "Test description"
+                    },
+                }
+            }
+            response = self.client.post(url, data, format="json")
+            print(response)
         pass
