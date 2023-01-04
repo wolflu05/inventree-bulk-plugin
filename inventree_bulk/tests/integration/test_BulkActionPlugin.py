eebf096f3596866fb8147fadf1f865320429e270
wolflu05 <76838159+wolflu05@users.noreply.github.com>
2023-01-04 16:21:21 +0100

    debug: tests

--- inventree_bulk/tests/integration/test_BulkActionPlugin.py
+++ inventree_bulk/tests/integration/test_BulkActionPlugin.py
@@ -1,5 +1,5 @@
 from django.urls import reverse
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from rest_framework.test import APITestCase
 
 from plugin import registry
@@ -15,23 +15,32 @@ class BulkActionPluginTestCase(TestCase):
 
 
 class BulkActionPluginAPITestCase(APITestCase):
+    @override_settings(PLUGIN_TESTING_SETUP=True)
     def test_url_parse(self):
-        print(registry.plugins)
-        print(registry.plugins_full)
-        print(registry.get_plugin('bulkaction'))
-        with self.assertRaises(Exception):
-            url = reverse("bulkaction:parse")
-            data = {
-                "version": "0.1.0",
-                "input": {},
-                "templates": [],
-                "output": {
-                    "generate": {
-                        "name": "Test",
-                        "description": "Test description"
-                    },
-                }
+        print("PLUGIN_BEFORE_RELOAD=", registry.get_plugin("bulkaction"))
+        registry.reload_plugins(True)
+
+        from django.conf import settings
+        for k in ["PLUGIN_TESTING_SETUP", "TESTING_ENV", "PLUGIN_TESTING"]:
+            print(f"{k}={getattr(settings, k, ' - Not Defined - ')}")
+        print("PLUGIN=", registry.get_plugin("bulkaction"))
+
+        from django import urls
+        url_resolver = urls.get_resolver(urls.get_urlconf())
+        print(url_resolver.namespace_dict.keys())
+        print(url_resolver.namespace_dict["plugin"][1].namespace_dict.keys())
+        url = reverse("plugin:bulkaction:parse")
+        data = {
+            "version": "0.1.0",
+            "input": {},
+            "templates": [],
+            "output": {
+                "generate": {
+                    "name": "Test",
+                    "description": "Test description"
+                },
             }
-            response = self.client.post(url, data, format="json")
-            print(response)
+        }
+        response = self.client.post(url, data, format="json")
+        print(response)
         pass
