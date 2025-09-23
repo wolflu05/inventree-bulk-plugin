eefe380580cd8b629266b464d53ad7ae60ebea0c
wolflu05 <76838159+wolflu05@users.noreply.github.com>
2025-09-23 20:52:07 +0200

    bump version in frontend

--- inventree_bulk_plugin/frontend/src/utils/constants.ts
+++ inventree_bulk_plugin/frontend/src/utils/constants.ts
@@ -1,6 +1,6 @@
 import { BulkDefinitionSchema } from "./types";
 
-export const schemaVersion = "1.4.2";
+export const schemaVersion = "1.4.3";
 
 export const defaultSchema: BulkDefinitionSchema = {
   version: schemaVersion,
